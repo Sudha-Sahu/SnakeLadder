8cde33f77583885fa0aae408e7e90120333e8bc7
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-24 01:26:18 +0530

    commit to UC3 checking if user gets a ladder or snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,17 +1,49 @@
 package com.game;
-import java.util.Random;
+import java.util.*;
 
 public class SnakeLadder {
-	
-	static int startPosition = 0;
-	
+
+	static final int ladder=1;
+	static final int snake=2;
+	static final int noPlay=0;
+	static final int startPosition = 0;
+
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Snake and Ladder game!!!!");
-		System.out.println("Initially the player is at start position that is : "+startPosition);
-		
-		Random random=new Random(); 
+		Random random=new Random();
+
+		int currentPosi=0;
 		int rollADie=random.nextInt(6)+1;
 		System.out.println("We get a number after a rolling a die that is: " + rollADie);
+		
+		int checkNextRoll=random.nextInt(3);
+		System.out.println("The player gets a condition: " + checkNextRoll);
+		
+		switch(checkNextRoll) {
+		case noPlay:
+			System.out.println("The player stay in same position that is: " + currentPosi);
+			break;
+		case ladder:
+			System.out.println("The player reached at a ladder.... ");
+			System.out.println("So you have to move forward from: " + currentPosi);
+			currentPosi += rollADie;
+			System.out.println("The current Position of player is: " + currentPosi);	
+			break;
+		case snake:
+			System.out.println("The player reached at a snake.... ");
+			System.out.println("So you have to move Backward from: " + currentPosi);
+			currentPosi -= rollADie;
+			if( currentPosi >= 0) {
+				System.out.println("The current Position of player is: " + currentPosi);
+			}
+			else {
+				currentPosi=0;
+				System.out.println("The current Position of player is: " + currentPosi);
+			}
+			break;
+		default:
+			System.out.println("Its default case");
+		}
 	}
 }
