9bb70b4429cb964b190dec16624d4553d28ce28e
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-24 01:07:33 +0530

    commit to UC1 to show starting position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,7 +4,9 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		
+		int startPosition = 0;
+		
 		System.out.println("Welcome to Snake and Ladder game!!!!");
-
+		System.out.println("Initially the player is at start position that is : " + startPosition);
 	}
 }
