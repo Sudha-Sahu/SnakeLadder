9a998c328079707c858516923eb9ee23e41ae4c8
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-24 01:15:30 +0530

    commit to UC2 getting a random number using Random function

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,17 @@
 package com.game;
+import java.util.Random;
 
 public class SnakeLadder {
-
+	
+	static int startPosition = 0;
+	
 	public static void main(String[] args) {
 		
-		int startPosition = 0;
-		
 		System.out.println("Welcome to Snake and Ladder game!!!!");
-		System.out.println("Initially the player is at start position that is : " + startPosition);
+		System.out.println("Initially the player is at start position that is : "+startPosition);
+		
+		Random random=new Random(); 
+		int rollADie=random.nextInt(6)+1;
+		System.out.println("We get a number after a rolling a die that is: " + rollADie);
 	}
 }
