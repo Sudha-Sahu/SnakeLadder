e31008bceb49853f70ef18ce22a7933db958ed1b
Sudha Sahu <sudhasahu52@gmail.com>
2022-01-25 16:47:38 +0530

    refactored the code in UC5

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,13 +1,13 @@
-package com.game;
+package com.Bridgelabz.SnakeLadder;
 import java.util.Random;
 
 public class SnakeLadder {
 
-	static final int ladder=1;
-	static final int snake=2;
-	static final int noPlay=0;
-	static final int startPosition = 0;
-	static final int finalPosition=100;
+	static final int LADDER=1;
+	static final int SNAKE=2;
+	static final int NOPLAY=0;
+	static final int STARTPOSITION = 0;
+	static final int FINALPOSITION=100;
 
 	public static void main(String[] args) {
 		
@@ -15,27 +15,27 @@ public class SnakeLadder {
 		Random random=new Random();
 
 		int currentPosi=0;
-		while(currentPosi <= finalPosition) {
+		while(currentPosi <= FINALPOSITION) {
 			int rollADie=random.nextInt(6)+1;
 			System.out.println("We get a number after a rolling a die that is: "+rollADie);
 			int checkNextRoll=random.nextInt(3);
 			System.out.println("The player gets a condition: "+checkNextRoll);
 			switch(checkNextRoll) {
-			case noPlay:
+			case NOPLAY:
 				System.out.println("The player stay in same position that is: "+currentPosi);
 				break;
-			case ladder:
+			case LADDER:
 				System.out.println("The player reached at a ladder.... ");
 				System.out.println("So you have to move forward from: "+currentPosi);
 				currentPosi += rollADie;
-				if(currentPosi == finalPosition)
+				if(currentPosi == FINALPOSITION)
 					System.out.println("The player reached at winning Position i.e. 100, So Player won the game!!!!!!! ");
-				else if( currentPosi > finalPosition) 
+				else if( currentPosi > FINALPOSITION) 
 					currentPosi -= rollADie;
 				else
 					System.out.println("The current Position of player is: "+currentPosi);	
 				break;
-			case snake:
+			case SNAKE:
 				System.out.println("The player reached at a snake.... ");
 				System.out.println("So you have to move Backward from: "+currentPosi);
 				currentPosi -= rollADie;
